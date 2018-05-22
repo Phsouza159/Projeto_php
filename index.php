9d342a1623089972d6f755e2c7a1faafeee17874
Phsouza159 <pauloh159@live.com>
2018-05-21 21:57:11 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+  include_once "banner_all.php";
+?>
+ <!DOCTYPE html>
+<html>
+  <head>
+    <meta charset='utf-8'>
+    <title>index</title>
+
+  </head>
+  <body>
+    <?php
+    //anexar banner
+    banner();
+    ?>
+
+     <p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dignissim nibh nec vehicula faucibus. Fusce sit amet pulvinar ante. Nulla semper eleifend lacus, in sodales mi vehicula vitae. Phasellus gravida urna eu arcu venenatis, vitae tincidunt dolor sodales. Duis convallis odio at mauris congue semper. Nam imperdiet, nulla nec mollis euismod, nibh nulla convallis ante, id ultrices augue erat ac mauris. Proin semper ante ex. Fusce at ornare tellus.
+</p>
+<p>
+Phasellus pulvinar interdum eros, nec volutpat massa facilisis sed. Aenean a pretium libero. Sed sagittis massa et augue pellentesque, tincidunt varius nunc aliquam. Sed in tincidunt urna. Praesent et vulputate augue. Duis lacus dolor, efficitur eu odio a, rutrum tempus dui. Ut nec rutrum risus. Nullam venenatis mollis felis, at semper quam ultrices quis. Nam nisl nisl, accumsan cursus eros vel, semper semper quam. Duis accumsan justo at magna congue viverra.
+</p>
+<p>
+Suspendisse aliquam tortor nec porttitor sollicitudin. Vestibulum dignissim vitae magna viverra vulputate. Duis nulla metus, dictum ut posuere non, rhoncus eget ipsum. Suspendisse nec tellus justo. Vestibulum vulputate, felis sed porta semper, velit mauris sollicitudin nibh, nec euismod turpis arcu id magna. Ut luctus odio vel velit posuere consequat. Vivamus non malesuada augue, ut egestas quam. Praesent risus dui, sagittis ut odio vel, condimentum scelerisque arcu. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras lacinia finibus erat et luctus. Phasellus id odio sit amet nulla vulputate vulputate at non diam. Nunc a purus tortor. Pellentesque vulputate imperdiet egestas. Donec libero leo, aliquet et scelerisque ac, eleifend sed dolor.
+</p>
+<p>
+Vivamus vel ex pulvinar, luctus lacus ut, auctor elit. Proin ac risus ipsum. Sed iaculis eleifend urna, sit amet sollicitudin ligula congue quis. Aliquam neque ante, congue at eros nec, convallis fermentum ante. Sed at ligula quis sem tempus ultricies a eget quam. Praesent mollis consectetur sapien, id aliquet elit condimentum in. Nullam non metus et dui scelerisque gravida eget malesuada nibh. Phasellus ac auctor augue. Vivamus malesuada aliquet posuere. Etiam lacinia, augue sed interdum laoreet, quam sapien fringilla nulla, at tincidunt sapien odio ut mi.
+</p>
+<p>
+Nulla magna mauris, vulputate sed tortor ac, pulvinar viverra nibh. Sed tincidunt nunc nec lectus congue facilisis. Duis non tortor sit amet eros fermentum elementum nec in quam. Proin odio odio, volutpat eget arcu sed, facilisis convallis mauris. Cras quis luctus felis. Aenean viverra consectetur dapibus. Suspendisse sed justo vel enim ullamcorper hendrerit et et augue.
+</p>
+<p>
+Nunc nec commodo tellus. Donec odio magna, sodales vestibulum tellus a, aliquam pellentesque nisl. Morbi blandit semper tellus, quis pulvinar neque finibus non. In tempor quam vel nisl dignissim fermentum. Vivamus fringilla massa molestie iaculis auctor. Pellentesque ipsum tellus, euismod sed lorem vitae, venenatis consequat nibh. Cras diam dui, aliquet nec lectus quis, mattis posuere magna. Nam ultrices vitae eros eget ultrices. Nulla facilisi. Pellentesque quis nunc vitae nisl bibendum consectetur et nec sapien.
+</p>
+<p>
+Mauris ac dui et risus luctus ornare. In semper purus in erat placerat, nec eleifend velit maximus. Sed sed luctus erat. Phasellus eu enim vel arcu tristique laoreet et aliquet libero. Curabitur sit amet vestibulum urna. Nulla sit amet libero ultricies arcu efficitur convallis vitae sit amet lacus. Curabitur auctor eu sem euismod congue. Duis venenatis tellus a augue commodo, id gravida turpis mattis.
+</p>
+<p>
+Proin a luctus libero, sed hendrerit enim. Sed porta tincidunt felis, sed consequat ipsum malesuada non. Sed diam quam, porttitor nec enim a, congue laoreet est. Cras quis tellus eu lectus convallis sagittis nec a magna. Morbi ullamcorper urna nibh, auctor ultrices felis blandit ut. Phasellus sed leo dignissim, gravida sem a, rhoncus mi. Ut scelerisque eget lorem nec iaculis. Quisque massa sapien, maximus vitae elit quis, elementum maximus mi. Aliquam interdum purus purus, ut eleifend ipsum vulputate quis. Donec maximus aliquet arcu ac auctor. Pellentesque eget velit sed nisl euismod egestas. Suspendisse potenti. Suspendisse placerat porta lacus sed scelerisque. Pellentesque vitae porta enim.
+</p>
+<p>
+Integer id accumsan augue. Maecenas non finibus sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget urna et purus rhoncus tristique. Donec eros turpis, feugiat vitae pellentesque nec, faucibus sit amet urna. Aliquam sollicitudin massa vel bibendum lobortis. Nunc aliquet ipsum sed lorem ullamcorper, quis auctor ante hendrerit. Vestibulum eget tristique sapien, ut vulputate purus. Vestibulum quam sem, scelerisque ac laoreet vitae, vulputate nec lectus. Quisque est ligula, interdum eu imperdiet vitae, congue sed ante. Morbi porttitor metus viverra felis tincidunt pharetra.
+</p>
+<p>
+Donec quis sodales tellus. Sed nec nisl sem. Praesent felis libero, molestie sit amet libero vitae, auctor lacinia tellus. Aliquam varius libero vitae maximus fermentum. Aliquam viverra massa eget pretium pulvinar. Etiam sed velit vel nisi aliquam maximus. Maecenas et condimentum ex. Pellentesque blandit quam urna, sit amet aliquet mi facilisis sit amet. Nulla fringilla lorem dolor, ac lobortis augue dapibus ac. Sed tincidunt lorem tincidunt fermentum varius. Nulla non quam cursus, placerat velit vestibulum, blandit metus. Aenean bibendum egestas orci eget luctus.
+</p>
+
+
+
+  </body>
+</html>
